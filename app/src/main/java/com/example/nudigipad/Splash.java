4746c696819abfec44fb0bed38dc773d3124c8f9
Voodoo-exe <iamkondur@gmail.com>
2021-01-03 16:59:06 +0530

    Initial commit

---
+++ app/src/main/java/com/example/nudigipad/Splash.java
@@ -0,0 +1,51 @@
+package com.example.nudigipad;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Splash extends AppCompatActivity {
+    FirebaseAuth fAuth;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        fAuth = FirebaseAuth.getInstance();
+
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                //check if user is logged in
+                if (fAuth.getCurrentUser() != null) {
+                    startActivity(new Intent(getApplicationContext(),MainActivity.class));
+
+                }else {
+                    //create new anonymous account
+                    fAuth.signInAnonymously().addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                        @Override
+                        public void onSuccess(AuthResult authResult) {
+                            Toast.makeText(Splash.this, "Logged in as Temporary User", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                            finish();
+                        }
+                    });
+                }
+
+
+            }
+        }, 2000);
+    }
+}
